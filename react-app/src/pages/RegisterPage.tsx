8f8a638b95786ab2081d68e15509366d3c31d1b3
W1ndrunner <lewislanek3264@gmail.com>
2024-07-14 18:11:00 +0100

    First iteration of Register Page

--- react-app/src/pages/RegisterPage.tsx
+++ react-app/src/pages/RegisterPage.tsx
@@ -13,13 +13,14 @@ import {
   Image,
   extendTheme,
 } from "@chakra-ui/react";
-import {FieldValues, SubmitHandler, useForm} from "react-hook-form";
-import{ useNavigate } from "react-router-dom";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
 
 const theme = extendTheme({
   colors: {
     brand: {
       text: "#FEFFFE",
+      button: "#C25BD7",
     },
     gradients: {
       blackToPurple: "linear(to-br, #000000, #5A007D)",
@@ -32,7 +33,7 @@ const RegisterPage = () => {
     handleSubmit,
     formState: { errors },
   } = useForm();
-  
+
   return (
     <ChakraProvider theme={theme}>
       <Flex
@@ -42,7 +43,11 @@ const RegisterPage = () => {
         bgGradient={theme.colors.gradients.blackToPurple}
       >
         <Box position="absolute" top={10} width="full" textAlign="center">
-          <Text bgGradient="linear(to-l, #7928CA, #FF0080)" fontSize="6xl" fontWeight="extrabold">
+          <Text
+            bgGradient="linear(to-l, #7928CA, #FF0080)"
+            fontSize="6xl"
+            fontWeight="extrabold"
+          >
             Spotinova
           </Text>
         </Box>
@@ -53,8 +58,57 @@ const RegisterPage = () => {
           boxShadow="xl"
           maxWidth="400px"
           width="full"
+        >
+          <form
+            onSubmit={handleSubmit((data) => {
+              console.log(data);
+            })}
           >
-
+            <VStack spacing={6}>
+              <FormControl textAlign="left">
+                <FormLabel color="brand.text" fontSize="lg" fontWeight="bold">
+                  Register
+                </FormLabel>
+                <FormLabel color="brand.text" fontSize="sm" mt={4}>
+                  E-mail
+                </FormLabel>
+                <Input
+                  type="email"
+                  placeholder="Enter your email"
+                  {...register("email", { required: true })}
+                />
+                {errors.email?.type === "required" && (
+                  <span>Email is required</span>
+                )}
+                <FormLabel color="brand.text" fontSize="sm" mt={4}>
+                  Password
+                </FormLabel>
+                <Input
+                  type="password"
+                  placeholder="Enter your password"
+                  {...register("password", { required: true })}
+                />
+                {errors.password?.type === "required" && (
+                  <span>Password is required</span>
+                )}
+              </FormControl>
+              <Button
+                color="white"
+                bg="brand.button"
+                width="full"
+                borderRadius="30px"
+                type="submit"
+              >
+                Sign Up
+              </Button>
+              <Text color="brand.text" fontSize="sm">
+                Already have an account?{" "}
+                <Link color = "brand.button" fontWeight="semibold" href="/login">
+                  Log in
+                  </Link>
+              </Text>
+            </VStack>
+          </form>
         </Box>
       </Flex>
     </ChakraProvider>
