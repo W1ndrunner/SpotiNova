77cea0919ea05bd935ab56adae9c313c2e969a78
W1ndrunner <lewislanek3264@gmail.com>
2024-07-17 19:04:36 +0100

    Set up DB

--- react-app/app.cjs
+++ react-app/app.cjs
@@ -3,3 +3,23 @@ const pg = require('pg');
 const app = express();
 const fs = require('fs');
 const cors = require('cors');
+require('dotenv').config();
+
+
+// Database connection details with SSL
+const pool = new pg.Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASS,
+    port: process.env.DB_PORT,
+    ssl: {
+        ca: fs.readFileSync('ca-certificate.crt').toString(),
+    },
+
+});
+
+// Starts Express server on port 3000
+app.listen(3000, () => {
+    console.log('Server listening on port 3000');
+});
